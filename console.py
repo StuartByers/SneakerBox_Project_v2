c9199f6cc3e42bbc43cbccb9904063692af4fe5c
StuartByers <stuart.byers@outlook.com>
2023-03-30 08:29:05 +0100

    updated visuals

--- console.py
+++ console.py
@@ -18,20 +18,20 @@ brand_repository.save(brand_3)
 brand_4 = Brand("Yeezy")
 brand_repository.save(brand_4)
 
-sneaker_1 = Sneaker("Jordan 1", brand_2, 165, "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/zzmfk3kwuw3ktkilxfnp/air-jordan-1-mid-shoes-MsflqZ.png", False)
+sneaker_1 = Sneaker("Jordan 1", brand_2, 165, "https://cms-cdn.thesolesupplier.co.uk/2022/03/air-jordan-1-high-og-lost-found-dz5485-612_w1024_h1024_pad_.jpg.webp", False)
 sneaker_repository.save(sneaker_1)
 
-sneaker_2 = Sneaker("Dunk", brand_1, 100, "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/zzmfk3kwuw3ktkilxfnp/air-jordan-1-mid-shoes-MsflqZ.png", False)
+sneaker_2 = Sneaker("Dunk", brand_1, 100, "https://cms-cdn.thesolesupplier.co.uk/2020/05/nike-dunk-low-retro-black-white_w1024_h1024_pad_.jpg.webp", False)
 sneaker_repository.save(sneaker_2)
 
-sneaker_3 = Sneaker("350 v2", brand_4, 180, "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/zzmfk3kwuw3ktkilxfnp/air-jordan-1-mid-shoes-MsflqZ.png", False)
+sneaker_3 = Sneaker("350 v2", brand_4, 180, "https://cms-cdn.thesolesupplier.co.uk/2018/10/Yeezy-Boost-350-V2-Zebra-Restock_w1024_h1024_pad_.jpg.webp", False)
 sneaker_repository.save(sneaker_3)
 
-sneaker_4 = Sneaker("UltraBoost", brand_3, 140, "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/zzmfk3kwuw3ktkilxfnp/air-jordan-1-mid-shoes-MsflqZ.png", False)
+sneaker_4 = Sneaker("UltraBoost", brand_3, 140, "https://cms-cdn.thesolesupplier.co.uk/2020/12/adidas-ultra-boost-10-black-reflective_w1024_h1024_pad_.jpg.webp", False)
 sneaker_repository.save(sneaker_4)
 
-sneaker_5 = Sneaker("Jordan 3", brand_2, 190, "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/zzmfk3kwuw3ktkilxfnp/air-jordan-1-mid-shoes-MsflqZ.png", False)
+sneaker_5 = Sneaker("Jordan 3", brand_2, 190, "https://cms-cdn.thesolesupplier.co.uk/2022/05/air-jordan-3-white-cement-reimagined-dn3707-100-1_w1024_h1024_pad_.jpg.webp", False)
 sneaker_repository.save(sneaker_5)
 
-sneaker_6 = Sneaker("700", brand_4, 250, "https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/zzmfk3kwuw3ktkilxfnp/air-jordan-1-mid-shoes-MsflqZ.png", False)
+sneaker_6 = Sneaker("700", brand_4, 250, "https://cms-cdn.thesolesupplier.co.uk/2022/10/yeezy-boost-700-gs-wave-runner-fu9005_w1024_h1024_pad_.jpg.webp", False)
 sneaker_repository.save(sneaker_6)
